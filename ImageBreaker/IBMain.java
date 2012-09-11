3ce85a475403e10db9387aa3f305d63539704162
Chris Bradel <ChrisBradel14@gmail.com>
2012-09-11 19:16:20 -0400

    Made it a command line program for fun.
    Call the jar with arguments: [image file name] [size of a side, eg:
    1024]
    I also added a simple counter so you know where it is in processing.
    
    Nothing special, just some java practice.

--- ImageBreaker/IBMain.java
+++ ImageBreaker/IBMain.java
@@ -1,40 +1,52 @@
-import java.awt.image.BufferedImage;
-import java.io.IOException;
+   import java.awt.image.BufferedImage;
+   import java.io.IOException;
+   import java.io.File;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFileChooser;
-import javax.swing.SwingUtilities;
+   import javax.imageio.ImageIO;
+   import javax.swing.JFileChooser;
+   import javax.swing.SwingUtilities;
 
 /**
  * Breaks images into several different 1024x1024 images (as well as any remainder).
  * 
- * @author Allen - Messed with by Chris
+ * @author Allen - Messed with by Chris to make it read a file from its arguments.
  * 
  */
-public class IBMain implements Runnable{
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		 SwingUtilities.invokeLater(new IBMain()); //Complying with Swing's threading policy.
-		 //http://docs.oracle.com/javase/6/docs/api/javax/swing/package-summary.html#threading
-	}
-
-	@Override
-	public void run() {
-		JFileChooser fc = new JFileChooser();
-		ImageSlicer imgSlicer;
-		if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
-			BufferedImage img = null;
-			try {
-			    img = ImageIO.read(fc.getSelectedFile());
-			} catch (IOException e) {
-				System.exit(1);
-			}
-			imgSlicer = new ImageSlicer(img, fc.getName(fc.getSelectedFile()));			
-			imgSlicer.slice(1024, 1024);			
-		}
-		
-	}
-}
+   public class IBMain {
+   
+   /**
+    * @param args
+    */
+      public static void main(String[] args) {
+         String path = "";
+      	int size = 1024;
+			
+         try {
+            path = args[0];
+				size =  Integer.parseInt(args[1]);
+         } 
+            catch ( ArrayIndexOutOfBoundsException e ) 
+            {
+               System.out.println("Make sure you have an image filename as the first argument,\nand the size of a side as the second.");
+               System.exit(1);
+            }
+      	
+         File f = new File(path);
+      
+         BufferedImage img = null;
+      
+         try {
+            img = ImageIO.read(f);
+         }	            
+            catch (IOException e) {
+               System.out.println("Problem reading file.");          
+               System.exit(1);
+            }
+      	
+         path = path.substring(path.lastIndexOf('/') + 1);
+               	
+         ImageSlicer imgSlicer;
+         imgSlicer = new ImageSlicer(img, path);
+         imgSlicer.slice(size, size);	
+      }
+   }
--- ImageBreaker/ImageSlicer.java
+++ ImageBreaker/ImageSlicer.java
@@ -1,76 +1,86 @@
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-import javax.swing.JOptionPane;
+   import java.awt.image.BufferedImage;
+   import java.io.File;
+   import java.io.IOException;
+   import javax.imageio.ImageIO;
+   import javax.swing.JOptionPane;
 
 /**
  * 
  * @author Allen - Also messed with by Chris
  *
  */
-public class ImageSlicer {
-	BufferedImage img;
-	String[] fileInfo;
-	
-	public ImageSlicer(BufferedImage i, String filename){
-		img = i;
-		fileInfo = filename.split("\\.");
-	}
-
-	/**
-	 * Breaks the given image up and saves the sub images to the user's desktop.
-	 * @param width Width of Subimages
-	 * @param height Height of Subimages
-	 */
-	public void slice(int width, int height){		
-		int x = img.getWidth()/width;
-		int y = img.getHeight()/height;
-		
-		if(x == 0 && y == 0){
-			infoBox("SubImages are larger than original!", "Error");
-			System.exit(2);
-		}
-		if (img.getWidth()%width > 0){
-			++x;
-		}
-		if (img.getHeight()%height > 0){
-			++y;
-		}
-		
-		BufferedImage[][] images = new BufferedImage[x][y];
-		File outputfile = null;
-		for (int j = 0; j < y; ++j){
-			for (int i = 0; i < x; ++i){
-				int w = width;
-				int h = height;
-				if (i+1 == x && img.getWidth()%width != 0){
-					w = img.getWidth()%width;					
-				}
-				if (j+1 == y && img.getHeight()%height != 0){
-					h = img.getHeight()%height;			
-				}
-				
-				images[i][j] = img.getSubimage(i*width,j*height, w, h);
-				
-				try {		
-					//This will save to user's desktop. Normally it defaults to where the JAR is saved.
-				    outputfile = new File(System.getProperty("user.home") + "/Desktop\\"+fileInfo[0]+"_"+i+"-"+j+"."+fileInfo[fileInfo.length-1]);
-				    ImageIO.write(images[i][j], fileInfo[fileInfo.length-1], outputfile);//Returns true if save succesful			    
-				} catch (IOException e) {
-					infoBox("Save failed!", "Error");
-				}
-			}
-		}
-		try {			
-			infoBox("Pictures saved to: "+outputfile.getCanonicalPath(), "Saved");
-		} catch (IOException e) {
-			infoBox("Pictures saved", "Saved");
-		}
-	}
-	
-	private void infoBox(String infoMessage, String title)
-    {
-        JOptionPane.showMessageDialog(null, infoMessage, title, JOptionPane.INFORMATION_MESSAGE);
-    }
-}
+   public class ImageSlicer {
+      BufferedImage img;
+      String[] fileInfo;
+   
+      public ImageSlicer(BufferedImage i, String filename){
+         img = i;
+         fileInfo = filename.split("\\.");
+      }
+   
+   /**
+    * Breaks the given image up and saves the sub images to the user's desktop.
+    * @param width Width of Subimages
+    * @param height Height of Subimages
+    */
+      public void slice(int width, int height){		
+         int x = img.getWidth()/width;
+         int y = img.getHeight()/height;
+      
+         if(x == 0 && y == 0){
+            infoBox("SubImages are larger than original!", "Error");
+            System.exit(2);
+         }
+         if (img.getWidth()%width > 0){
+            ++x;
+         }
+         if (img.getHeight()%height > 0){
+            ++y;
+         }
+      	
+         int count = 1;    
+      
+         BufferedImage[][] images = new BufferedImage[x][y];
+         File outputfile = null;
+         for (int j = 0; j < y; ++j){
+            for (int i = 0; i < x; ++i){
+               int w = width;
+               int h = height;
+               if (i+1 == x && img.getWidth()%width != 0){
+                  w = img.getWidth()%width;					
+               }
+               if (j+1 == y && img.getHeight()%height != 0){
+                  h = img.getHeight()%height;			
+               }
+            
+               images[i][j] = img.getSubimage(i*width,j*height, w, h);
+            
+               try {		
+               //This will save to where the JAR is saved, in the folder "output".
+                  String stupidlongthing = ClassLoader.getSystemClassLoader().getResource(".").getPath().replaceAll("%20", " ")+"\\output\\"+fileInfo[0]+"_"+i+"-"+j+"."+fileInfo[fileInfo.length-1];
+                  stupidlongthing = stupidlongthing.replaceAll("file:/", "").replaceAll("ImageBreaker.jar\\!", "");      
+               	
+                  outputfile = new File(stupidlongthing);
+               
+                  System.out.println(count+"/"+(x*y));
+                  count++;
+                  File directory = outputfile.getParentFile();
+                  directory.mkdirs();
+                  
+                  ImageIO.write(images[i][j], fileInfo[fileInfo.length-1], outputfile);//Returns true if save succesful			    
+               	
+               } 
+                  catch (IOException e) {
+                     //infoBox("Save failed!", "Error");
+                  }
+            }
+         }
+         
+         System.out.println("Saving completed.");
+      }
+   
+      private void infoBox(String infoMessage, String title)
+      {
+         JOptionPane.showMessageDialog(null, infoMessage, title, JOptionPane.INFORMATION_MESSAGE);
+      }
+   }
